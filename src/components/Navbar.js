adc8583574f620024c3b1ac6cf402599b8e5c6ea
Mouzin Gulzar <mouzinmonis@gmail.com>
2021-11-12 23:47:55 +0530

    Removing Home link from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,11 +23,6 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
                 <Link className="nav-link active" to="/about">
                   About
                 </Link>
